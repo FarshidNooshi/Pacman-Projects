f5255cd5b85a01b3d7d491f3586f30b656dc3852
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-15 17:16:38 +0430

    add space, test commit

--- Pacman-Search/src/searchAgents.py
+++ Pacman-Search/src/searchAgents.py
@@ -46,7 +46,7 @@ class GoWestAgent(Agent):
     "An agent that goes West until it can't."
 
     def getAction(self, state):
-        "The agent receives a GameState (defined in pacman.py)."
+        "The agent receives a GameState (defined in pacman.py). "
         if Directions.WEST in state.getLegalPacmanActions():
             return Directions.WEST
         else:
